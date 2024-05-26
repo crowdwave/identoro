6ec645631a0ba43bc76e07394de2c67072813bf2
crowdwave <andrew.stuart@supercoders.com.au>
2024-05-26 15:24:45 +1000

    Update ui_react.ts

--- ui_react.ts
+++ ui_react.ts
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom'; 
 
 interface AuthState {
   username: string;
